aefae398470245f3581d819c7e77d4eb6482725f
FanoDevs <hasseymoi@gmail.com>
2023-04-23 21:36:07 -0400

    Add files via upload

---
+++ scoreBoard.js
@@ -0,0 +1,36 @@
+let homeScoreEl = document.getElementById("home-score")
+let guestScoreEl = document.getElementById("guest-score")
+let homeScore = 0
+let guestScore = 0
+
+function reset() {
+    homeScore = 0
+    guestScore = 0
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
+}
+
+function increment_home1() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+function increment_home2() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+function increment_home3() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+function increment_guest1() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+function increment_guest2() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+function increment_guest3() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
